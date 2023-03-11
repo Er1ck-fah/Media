87bf545f0103f9296086dcc539d8314f74ba00ca
Erick KONGNE FAH <erickkongne@gmail.com>
2023-03-12 00:45:59 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
+//Components
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+
+//Pages
+import Home from "./pages/home/Home";
+import Add from "./pages/add/Add";
+import Gig from "./pages/gig/Gig";
+import Gigs from "./pages/gigs/Gigs";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import MyGigs from "./pages/myGigs/MyGigs";
+import Orders from "./pages/orders/Orders";
+//Styles
+import "./app.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+      ],
+    },
+  ]);
 
   return (
     <div className="App">
-  Hello world
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
