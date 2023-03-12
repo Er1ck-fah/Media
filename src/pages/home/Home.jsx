453b940d4992951139beb20ac3ca2c62e8e8b0ac
Erick KONGNE FAH <erickkongne@gmail.com>
2023-03-12 02:02:53 +0100

    Home v1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
 const Home = () => {
-  return <div className="home">Home</div>;
+  return <div className="home">
+    <Featured/>
+    <TrustedBy/>
+  </div>;
 };
 
 export default Home;
